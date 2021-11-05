6dbca3facd00e059f6bb7ed640ca122dbd383dda
Gabriel Igliozzi <gaboiglio@gmail.com>
2021-11-04 22:55:40 -0400

    Started debug file and makefile

--- src/Debug.c
+++ src/Debug.c
@@ -1,3 +1,19 @@
 #include "Debug.h"
 
+void disassembleCodeBuffer(CodeBuffer* buffer) {
+
+}
+
+int disassembleOpCode(CodeBuffer* buffer, int offset) {
+    uint8_t opcode = buffer->data[offset];
+    
+    switch (opcode) {
+        case OP_CONSTANT: break;
+        case OP_ADD: break;
+        default:
+            fprintf(stderr, "[ERROR] Unknown upcode: %d", opcode);
+            return offset + 1;
+    }
+    return 0;
+}
 
--- src/Debug.h
+++ src/Debug.h
@@ -1,6 +1,7 @@
 #ifndef debug_h
 #define debug_h
 
+#include <stdio.h>
 #include "CodeBuffer.h"
 
 void disassembleCodeBuffer(CodeBuffer* buffer);
