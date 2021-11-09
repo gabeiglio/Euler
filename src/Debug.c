141c8c2ded3e22986b59338c9cc53074e574cb7b
Gabriel Igliozzi <gaboiglio@gmail.com>
2021-11-08 19:48:32 -0500

    Implemented initial opcodes to disassembler

--- src/Debug.c
+++ src/Debug.c
@@ -1,15 +1,24 @@
 #include "Debug.h"
 
-void disassembleCodeBuffer(CodeBuffer* buffer) {
-
+static int printSimpleOpCode(const char* name, int offset) {
+    printf("%s", name);
+    return offset + 1;
 }
 
-int disassembleOpCode(CodeBuffer* buffer, int offset) {
+static int printConstantOpCode(const char* name, CodeBuffer* buffer, int offset) {
+    printf("OP_CONSTANT: %i\n", buffer->values.values[(int)buffer->data[offset + 1]]);
+    return offset + 1;
+}
+
+static int disassembleOpCode(CodeBuffer* buffer, int offset) {
     uint8_t opcode = buffer->data[offset];
-    
+   
     switch (opcode) {
-        case OP_CONSTANT: break;
-        case OP_ADD: break;
+        case OP_CONSTANT: return printConstantOpCode("OP_CONSTANT", buffer, offset);
+        case OP_ADD:      return printSimpleOpCode("OP_ADD", offset);
+        case OP_MULTIPLY: return printSimpleOpCode("OP_MULTIPLY", offset);
+        case OP_DIVIDE:   return printSimpleOpCode("OP_DIVIDE", offset);
+        case OP_SUBSTRACT:return printSimpleOpCode("OP_SUBSTRACT", offset);
         default:
             fprintf(stderr, "[ERROR] Unknown upcode: %d", opcode);
             return offset + 1;
@@ -17,3 +26,10 @@ int disassembleOpCode(CodeBuffer* buffer, int offset) {
     return 0;
 }
 
+void disassembleCodeBuffer(CodeBuffer* buffer) {
+    printf("\n\n== STACK TRACE ==\n");
+    for (int offset = 0; offset < buffer->count;) {
+        printf("\n%i-> ", offset);
+        offset = disassembleOpCode(buffer, offset);
+    }
+}
--- src/Debug.h
+++ src/Debug.h
@@ -5,6 +5,5 @@
 #include "CodeBuffer.h"
 
 void disassembleCodeBuffer(CodeBuffer* buffer);
-int disassembleOpCode(CodeBuffer* buffer, int offset);
 
 #endif
